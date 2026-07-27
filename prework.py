43558eb9bd9b60005ca1eca84320aeba6b8243df
Sarai-Zarate <szarate627@gmail.com>
2026-07-27 07:23:31 -0500

    Added a note

--- prework.py
+++ prework.py
@@ -2,6 +2,7 @@ customer_name = input("Enter your name in the field:" )
 purchase_total = float(input("Type your purchase total here:" ))
 shipping_total = 12.00
 
+#free shipping with a purchase of $50 or more. A specialized message prints at different purchase totals. 
 if purchase_total >= 50.00:
     shipping_toal = 0.00
     print(customer_name + ", hooray! You qualify for free shipping and this month's freebie with your $" + str(purchase_total) + " purchase!")
